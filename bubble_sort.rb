6377ac7cd4ffe030c597d01d0bf7238c97378097
ezeilo-su <ezeilo.su@gmail.com>
2020-04-22 18:22:26 +0100

    Implement yield and block on bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,32 @@
 def  bubble_sort(arr)
+  yield(arr)
+  return arr
+end
+
+unsorted = [4,7,0,10,1]
+
+sorted = bubble_sort(unsorted) do |arr|
+  is_sorted = false
+  length_to_sort = arr.length-1
+  debug = 0
+  while !is_sorted
+    is_sorted = true
+    n = 0
+    until n == length_to_sort
+      if arr[n] > arr[n+1]
+        arr[n], arr[n+1] = arr[n+1], arr[n]   #swap the contents of two indexes
+        is_sorted = false
+      end
+      n += 1
+    end
+    length_to_sort -= 1
+  end
+end
+
+puts sorted.to_s
+
+=begin
+def  bubble_sort(arr)
   is_sorted = false
   length_to_sort = arr.length-1
   debug = 0
@@ -19,3 +47,4 @@ def  bubble_sort(arr)
 end
 
 puts bubble_sort([4,7,0,10,1])
+=end
