315d7f92727d299011afdfa20efd914fcfd4bfb4
ezeilo-su <ezeilo.su@gmail.com>
2020-04-22 23:37:29 +0100

    Fix Linter bugs

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,19 @@
-def  bubble_sort(arr)
+def bubble_sort(arr)
   yield(arr) if block_given?
-  return arr
+  arr
 end
 
-unsorted_arr = [4,7,0,10,1]
+unsorted_arr = [4, 7, 0, 10, 1]
 
 sorted = bubble_sort(unsorted_arr) do |arr|
   is_sorted = false
-  length_to_sort = arr.length-1
-  debug = 0
-  while !is_sorted
+  length_to_sort = arr.length - 1
+  while is_sorted == false
     is_sorted = true
     n = 0
     until n == length_to_sort
-      if arr[n] > arr[n+1]
-        arr[n], arr[n+1] = arr[n+1], arr[n]   #swap the contents of two indexes
+      if arr[n] > arr[n + 1]
+        arr[n], arr[n + 1] = arr[n + 1], arr[n]
         is_sorted = false
       end
       n += 1
